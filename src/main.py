ad9f5530edeffa7331c9920a5e41b9459c2c4f03
ravillagopinath <ravilladhaneesh@gmail.com>
2024-10-13 21:45:30 +0530

    Add code to find file extensions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 import os
 
-#local
-'''
-repo_path = os.getcwd()
-repo_name = 'dummy'
-'''
+
 
 
 print('----------------------')
@@ -14,19 +10,50 @@ print('----------------------')
 repo_name = os.environ.get('REPO_NAME')
 repo_path = os.environ.get('REPO_PATH')
 repo_url = os.environ.get('REPO_URL')
+
+
+
+#local
+'''
+repo_path = os.getcwd()
+repo_name = 'ravilladhaneesh/workflow-test'
+repo_path = "C:\\Users\\Gopinath\\OneDrive\\Documents\\Dhaneesh folder\\personal project\\workflow-test"
+repo_url = 'https://github.com/ravilladhaneesh/workflow-test'
+repo_path = "C:\\Users\\Gopinath\\OneDrive\\Documents\\Dhaneesh folder\\AI-ML"
+'''
+
+
 print(repo_name)
 print('---------------------')
 print(repo_path)
 print('---------------------')
 print(repo_url)
 print('---------------------')
-    
+
+
+
+
 excluded_dirs = {'.git', '.github', 'scraper'}
-        
+USERNAME, REPO = repo_name.split('/')
+
+#file_extensions = ['py', 'css', 'html', 'txt', 'java']
+
+file_extensions = {}
+
 for dirpath, dirnames, filenames in os.walk(repo_path):
     dirnames[:] = [d for d in dirnames if d not in excluded_dirs]
             
-    print(f"\nDirectory: {dirpath}")
+    #print(f"\nDirectory: {dirpath}")
             
     for file in filenames:
-        print(f"  File: {file}")
+        #print(f"  File: {file}")
+        if not file.startswith('.git'):
+            extension = file.split('.')[-1]
+            if extension in file_extensions:
+                file_extensions[extension] += 1
+            else:
+                file_extensions[extension] = 1
+
+for extension, value in file_extensions.items():
+    print(extension, value)
+                
